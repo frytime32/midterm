5fa9071dae38b295808feb1b855aecfbae9849af
Brandon Fry <frytime32@gmail.com>
2017-12-01 15:52:46 +0000

    database now working

--- models/Comments.js
+++ models/Comments.js
@@ -3,8 +3,20 @@ var CommentSchema = new mongoose.Schema({
   title: String,
   upvotes: {type: Number, default: 0},
 });
+var StoreSchema = new mongoose.Schema({
+  title: String,
+  upvotes: {type: Number, default: 0},
+  price:{type: Number, default: 0},
+  url: {type: String, default: "https://iphoneimei.net/images/attachment/2014/10/24/iphone5.png"},
+});
+
 CommentSchema.methods.upvote = function(cb) {
   this.upvotes += 1;
   this.save(cb);
 };
-mongoose.model('Comment', CommentSchema);
+StoreSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
+
+mongoose.model('Comment', StoreSchema);
